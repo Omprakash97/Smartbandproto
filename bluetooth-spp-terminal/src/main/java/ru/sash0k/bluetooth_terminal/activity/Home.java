8dd9d02287b750627fbe447cf7efc3a3b115bee5
unknown <r.omprakash1997@gmail.com>
2019-06-03 16:13:36 +0530

    Initial commit

---
+++ bluetooth-spp-terminal/src/main/java/ru/sash0k/bluetooth_terminal/activity/Home.java
@@ -0,0 +1,15 @@
+package ru.sash0k.bluetooth_terminal.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.sash0k.bluetooth_terminal.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
